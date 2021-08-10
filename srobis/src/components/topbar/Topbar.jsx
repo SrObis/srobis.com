f1e3a9b5061444d88a98f038d3d045b8d1b7a05b
SrObis <obis2001@gmail.com>
2021-08-10 09:30:45 +0200

    topbar

--- srobis/src/components/topbar/Topbar.jsx
+++ srobis/src/components/topbar/Topbar.jsx
@@ -3,7 +3,18 @@ import "./topbar.scss"
 export default function Topbar(){
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+
+                <div className="left">
+                    <a href="#intro" className="logo">Jordi Obis Farre</a>
+                </div>
+                
+                <div className="right">
+                    
+                </div>
+
+            </div>
+           
         </div>
     )
 }
