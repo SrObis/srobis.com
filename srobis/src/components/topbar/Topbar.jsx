d767f2461ac09985797f07042600f66fba49eeb0
SrObis <obis2001@gmail.com>
2021-08-13 20:51:59 +0200

    React Anim Hamburger menu

--- srobis/src/components/topbar/Topbar.jsx
+++ srobis/src/components/topbar/Topbar.jsx
@@ -1,16 +1,25 @@
 import "./topbar.scss"
+import {Mail} from "@material-ui/icons"
 
-export default function Topbar(){
+export default function Topbar({ menuOpen, setMenuOpen }){
     return (
-        <div className="topbar">
+        <div className={"topbar "+(menuOpen && "active")}>
             <div className="wrapper">
 
                 <div className="left">
                     <a href="#intro" className="logo">Jordi Obis Farre</a>
-                </div>
                 
+                    <div clasName="itemContainer">
+                        <Mail clasName="icon" />
+                        <span>jordi@srobis.com</span>
+                    </div>
+                </div>
                 <div className="right">
-                    
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span clasName="line1"></span>
+                        <span clasName="line2"></span>
+                        <span clasName="line3"></span>
+                    </div>
                 </div>
 
             </div>
