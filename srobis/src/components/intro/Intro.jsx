50efb094f4d30ff92dcb5a01e75f7977d32ea545
SrObis <obis2001@gmail.com>
2021-08-15 12:53:47 +0200

    animacion

--- srobis/src/components/intro/Intro.jsx
+++ srobis/src/components/intro/Intro.jsx
@@ -1,6 +1,18 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro(){
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Programador web"],
+        });
+    },[]);
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +24,7 @@ export default function Intro(){
                 <div className="wrapper">
                     <h2>Mi nombre es</h2>
                     <h1>Jordi Obis Farre</h1>
-                    <h3>Soy<span></span></h3>
+                    <h3>Soy <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                    <img src="assets/down.png" alt=""/>
